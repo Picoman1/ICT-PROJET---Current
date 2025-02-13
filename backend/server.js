8ecf3f54c1c92f2de4df7954fde0b91f67c9cbcb
Nikhil Francis <nikhilfrancis2001@gamil.com>
2025-02-14 00:11:00 +0530

    cors in server.js changed

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,11 @@ connectDB();
 connectCloudinary();
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+     origin: "https://ecommerce-frontend-whzj.onrender.com", 
+     methods: ["GET", "POST", "PUT", "DELETE"],
+     credentials: true
+   }));
 //end points
 app.use('/api/user',userRouter);
 app.use('/api/product',productRouter);
